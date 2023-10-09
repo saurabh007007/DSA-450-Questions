e903edbe96da82cdebb76ebac065e2061a993bde
Saurabh Yadav <116506457+saurabh007007@users.noreply.github.com>
2023-10-10 00:42:17 +0530

    Create cyclic_rotate_array.java

---
+++ Array/cyclic_rotate_array.java
@@ -0,0 +1,8 @@
+//Cyclic rotate array 
+public class cyclic_rotate_array{
+  public static void rotate_array(int arr[]){
+    int i=o;
+    int j=arr.length-1;
+    for(int 
+  public static void main(String arg[]){
+    
